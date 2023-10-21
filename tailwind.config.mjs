7c6ca6f702ca45f90fda710f78b32167ac381593
Hector Rodriguez <hector@hrod.dev>
2023-10-20 20:30:32 -0500

    add custom colorw

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,10 +4,10 @@ export default {
 	theme: {
 		extend: {},
 		colors: {
-			'light-red': 'hsl(0, 100%, 67%)',
-			'orangey-yellow': 'hsl(39, 100%, 56%)',
-			'green-teal': 'hsl(166, 100%, 37%)',
-			'cobalt-blue': 'hsl(234, 85%, 45%)',
+			'light-red': 'hsla(0, 100%, 67%,0.10)',
+			'orangey-yellow': 'hsla(39, 100%, 56%,0.10)',
+			'green-teal': 'hsla(166, 100%, 37%,0.10)',
+			'cobalt-blue': 'hsla(234, 85%, 45%,0.10)',
 			'light-slate-blue': 'hsl(252, 100%, 67%)',
 			'light-royal-blue': 'hsl(241, 81%, 54%)',
 			'violet-blue': 'hsla(256, 72%, 46%, 1)',
